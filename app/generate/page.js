57b309c345fa028d5a9cd72d7302b358db76946f
Gunjan Agrawal <gunjanagrawal582@gmail.com>
2025-06-08 15:15:08 +0530

    Update page.js

--- app/generate/page.js
+++ app/generate/page.js
@@ -53,11 +53,11 @@ const Generate = () => {
       redirect: "follow"
     };
 
-   const r = await fetch("http://localhost:3000/api/add", requestOptions)
+   const r = await fetch("/api/add", requestOptions)
    const result = await r.json()
    if(result.success){ 
      toast.success(result.message)
-     setLinks([])
+     setLinks([{ link: "", linktext: "" }])
      setpic("")
      sethandle("")
     }
